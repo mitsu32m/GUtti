8a6141b6d6dec70214ae6d4209489436fe04a0d9
Mitsu <m23110@g.metro-cit.ac.jp>
2025-12-08 12:01:31 +0900

    aa

--- testComplex.c
+++ testComplex.c
@@ -32,64 +32,6 @@ void testMakeCompRT() {
     assertEqualsComplex(ans, makeComp(1/sqrt(2), -1/sqrt(2)));
 }
 
-
-
-
-
-// ↑↑↑↑ ここまでを 3327 中野光喜 が記述(この範囲以外には追加しない)
-
-//////////////////////////////////////////////////////////////////////////////////////////
-
-// ↓↓↓↓ 3328 西口豪志 記述部(この範囲以外には追加しない)
-
-void testSecond() {
-    testStart("second");
-    assertEqualsDouble(sqrt(4.0), 2.0);
-    assertEqualsDouble(sqrt(9.0), 3.0);
-}
-void testMakeComp() {
-    complex ans;
-    testStart("makeComp");
-    // 1+2jを作成
-    ans = makeComp(1.0, 2.0);
-    assertEqualsDouble(ans.real, 1.0);
-    assertEqualsDouble(ans.image, 2.0);
-    // 3.4+5.6jを作成
-    ans = makeComp(3.4, 5.6);
-    assertEqualsDouble(ans.real, 3.4);
-    assertEqualsDouble(ans.image, 5.6);
-}
-void testAddComp() {
-    complex a, b, ans;
-
-    testStart("addComp");
-
-    // ---- Test 1: (1 + 2j) + (3 + 4j) = (4 + 6j) ----
-    a = makeComp(1.0, 2.0);
-    b = makeComp(3.0, 4.0);
-    ans = addComp(a, b);
-
-    assertEqualsDouble(ans.real, 4.0);
-    assertEqualsDouble(ans.image, 6.0);
-
-    // ---- Test 2: (3.4 + 5.6j) + (1.1 + 2.2j) = (4.5 + 7.8j) ----
-    a = makeComp(3.4, 5.6);
-    b = makeComp(1.1, 2.2);
-    ans = addComp(a, b);
-
-    assertEqualsDouble(ans.real, 4.5);
-    assertEqualsDouble(ans.image, 7.8);
-
-    // ---- Test 3: (0 + 0j) + (2.5 + -3.1j) = (2.5 + -3.1j) ----
-    a = makeComp(0.0, 0.0);
-    b = makeComp(2.5, -3.1);
-    ans = addComp(a, b);
-
-    assertEqualsDouble(ans.real, 2.5);
-    assertEqualsDouble(ans.image, -3.1);
-}
-
-
 // 8. 共役複素数 (虚部の符号が反転)
 void testConjComp() {
     complex a, ans;
@@ -220,6 +162,64 @@ void testPrintCompRT() {
     printf("\n");
 }
 
+
+
+// ↑↑↑↑ ここまでを 3327 中野光喜 が記述(この範囲以外には追加しない)
+
+//////////////////////////////////////////////////////////////////////////////////////////
+
+// ↓↓↓↓ 3328 西口豪志 記述部(この範囲以外には追加しない)
+
+void testSecond() {
+    testStart("second");
+    assertEqualsDouble(sqrt(4.0), 2.0);
+    assertEqualsDouble(sqrt(9.0), 3.0);
+}
+void testMakeComp() {
+    complex ans;
+    testStart("makeComp");
+    // 1+2jを作成
+    ans = makeComp(1.0, 2.0);
+    assertEqualsDouble(ans.real, 1.0);
+    assertEqualsDouble(ans.image, 2.0);
+    // 3.4+5.6jを作成
+    ans = makeComp(3.4, 5.6);
+    assertEqualsDouble(ans.real, 3.4);
+    assertEqualsDouble(ans.image, 5.6);
+}
+void testAddComp() {
+    complex a, b, ans;
+
+    testStart("addComp");
+
+    // ---- Test 1: (1 + 2j) + (3 + 4j) = (4 + 6j) ----
+    a = makeComp(1.0, 2.0);
+    b = makeComp(3.0, 4.0);
+    ans = addComp(a, b);
+
+    assertEqualsDouble(ans.real, 4.0);
+    assertEqualsDouble(ans.image, 6.0);
+
+    // ---- Test 2: (3.4 + 5.6j) + (1.1 + 2.2j) = (4.5 + 7.8j) ----
+    a = makeComp(3.4, 5.6);
+    b = makeComp(1.1, 2.2);
+    ans = addComp(a, b);
+
+    assertEqualsDouble(ans.real, 4.5);
+    assertEqualsDouble(ans.image, 7.8);
+
+    // ---- Test 3: (0 + 0j) + (2.5 + -3.1j) = (2.5 + -3.1j) ----
+    a = makeComp(0.0, 0.0);
+    b = makeComp(2.5, -3.1);
+    ans = addComp(a, b);
+
+    assertEqualsDouble(ans.real, 2.5);
+    assertEqualsDouble(ans.image, -3.1);
+}
+
+
+
+
 // ↑↑↑↑ ここまでを 3328 西口豪志 が記述(この範囲以外には追加しない)
 
 //////////////////////////////////////////////////////////////////////////////////////////
@@ -232,6 +232,7 @@ void testPrintCompRT() {
 int main() {
     // ↓↓↓↓ 33xx XXXX 記述部(この範囲以外には追加しない)
     testFirst();
+    testAddComp();
     // ↑↑↑↑ ここまでを 33xx XXXX が記述(この範囲以外には追加しない)
     testMakeCompRT();
     testConjComp();     // No.8
